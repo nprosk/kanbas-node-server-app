59098331b4bbddf913985358df5618adb3e0160f
nprosk <proskauer.n@northeastern.edu>
2023-11-20 21:23:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './lab5.js';
+import cors from "cors";
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import "dotenv/config";
+import session from "express-session";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
